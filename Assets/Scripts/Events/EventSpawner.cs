0f41d49805110e89a74f1b134b965834e9c0405b
joshcheung21 <joshss212312@gmail.com>
2019-02-02 10:42:57 -0700

    Event Spawner

---
+++ Assets/Scripts/Events/EventSpawner.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EventSpawner : MonoBehaviour {
+
+    public Boundaries boundaries;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void CheckForBoundaries()
+    {
+        
+    }
+    void GetRandomPos()
+    {
+        
+    }
+}
